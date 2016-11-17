1555de9130bd64a683dbde63f7c0e6f5f372ead3
jlarsm <jared.morris@ucalgary.ca>
2016-11-16 20:09:42 -0700

    Update WriteBuffer.java

--- WriteBuffer.java
+++ WriteBuffer.java
@@ -1,23 +1,37 @@
 package java.util.concurrent;
-
+import java.util.*;
 public class WriteBuffer<T extends Object> {
-	public boolean tso;
-	public Object[] array;
+	public boolean pso;
+	public Object[] buffer;
 	public boolean[] inQueue;
-	public dequeue<Object
+	public Deque<Object> storeQueue;
 	
 	
 	public WriteBuffer(boolean t, int n){
-		this.tso = t;
-		this.array= new Object[n];
+		this.pso = t;
+		this.buffer= new Object[n];
 		this.inQueue = new boolean[n];
 	}
+	
 	public Object load(int x){ //throws not found in buffer exception
-		Object tmp = array[x];
-		return tmp;
-	}
-	public void store(int x, Object v){
+		try{
+			Object tmp = buffer[x];
+			return tmp;
+		}
+		catch (IndexOutOfBoundsException e){
+			System.err.println("Error item not in buffer" + e.getMessage());
+			return -1;
+		}
 		
+	}
+	
+	public void store(int x, Object v){
+		if(this.pso == true){
+			
+		}
+		else{
+			
+		}
 	}
 
 }
