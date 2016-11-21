214f556681f2d84e58e5b53908d7b3c19c98ee3a
jlarsm <jared.morris@ucalgary.ca>
2016-11-21 16:58:29 -0700

    Update Processor.java

--- Processor.java
+++ Processor.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 import java.util.concurrent.*;
 public class Processor extends Thread {
 
@@ -30,8 +30,9 @@ public class Processor extends Thread {
 			buffer.store("turn"+Integer.toString(z), 0);
 			
 		}
-
+		
 		// <Entry Section>>
+		
 		for (int k = 0; k <= 8; k++)  // k is stored in a register in the processor
 		{
 			buffer.store("flag"+Integer.toString(i), k);
@@ -57,18 +58,31 @@ public class Processor extends Thread {
 				}
 				while (flag_j >= k && turn_k == i) ; //nop
 			}
-		}
+		}*/
 		//Critical Section
 		 
-		 
+		
 		for(int x=0; x<10;x++){
 			raceCondition[x] = i;  // fill raceCondition array with pid
+			
+			
+			// threads that start first (lower ID) have to wait longer
+			// (to increase probability of different values in raceCondition array
+			try {
+				Thread.currentThread().sleep(1000/(i+3));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}
+		
 		
 		
 		//<<Exit Section>>
 		buffer.store("flag"+Integer.toString(i), -1);
-		*/
+		
 	}
 	
 }
